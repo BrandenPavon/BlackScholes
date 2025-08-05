3027f4e85fe294a0a59c7d97e894fa55cf0a5418
bp <bp@bpsite.xyz>
2025-08-05 11:24:55 -0700

    fix: fix errors and use reference for formula

--- backtest.cpp
+++ backtest.cpp
@@ -6,26 +6,26 @@ struct OptionContract {
   double ttm;
   double riskfreeinterest;
   double volatility;
-}
+};
 
-double CallBlackScholesFormula(const struct * OC) {
-  double S = OC->spotprice;
-  double K = OC ->strikeprice;
-  double T = OC->ttm;
-  double r = OC->riskfreeinterest;
-  double V = OC->volatility;
-  d1 = (std::log(S/K) + (r+0.5*V*V) * T) / (V*std::sqrt(T));
-  d2 = d1 - V * std::sqrt(T);
+double CallBlackScholesFormula(const OptionContract & OC) {
+  double S = OC.spotprice;
+  double K = OC.strikeprice;
+  double T = OC.ttm;
+  double r = OC.riskfreeinterest;
+  double V = OC.volatility;
+  double d1 = (std::log(S/K) + (r+0.5*V*V) * T) / (V*std::sqrt(T));
+  double d2 = d1 - V * std::sqrt(T);
   return S * (0.5*std::erfc(-d1/std::sqrt(2.0))) - K * std::exp(-r*T) * (0.5*std::erfc(-d2/std::sqrt(2.0)));
 }
 
-double PutBlackScholesFormula(const struct * OC) {
-  double S = OC->spotprice;
-  double K = OC ->strikeprice;
-  double T = OC->ttm;
-  double r = OC->riskfreeinterest;
-  double V = OC->volatility;
-  d1 = (std::log(S/K) + (r+0.5*V*V) * T) / (V*std::sqrt(T));
-  d2 = d1 - V * std::sqrt(T);
+double PutBlackScholesFormula(const OptionContract & OC) {
+  double S = OC.spotprice;
+  double K = OC.strikeprice;
+  double T = OC.ttm;
+  double r = OC.riskfreeinterest;
+  double V = OC.volatility;
+  double d1 = (std::log(S/K) + (r+0.5*V*V) * T) / (V*std::sqrt(T));
+  double d2 = d1 - V * std::sqrt(T);
   return K * (0.5*std::erfc(-d1/std::sqrt(2.0))) - S * std::exp(-r*T) * (0.5*std::erfc(-d2/std::sqrt(2.0)));
 }
