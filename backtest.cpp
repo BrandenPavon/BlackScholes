3ea4469830bf12be3bbc66a5c1889d1fad92db89
bp <bp@bpsite.xyz>
2025-08-05 11:20:12 -0700

    feat: added blackscholes formula

--- backtest.cpp
+++ backtest.cpp
@@ -1,9 +1,31 @@
-
-double riskfreeinterest;
+#include <cmath>
 
 struct OptionContract {
   double spotprice;
   double strikeprice;
-  int ttm;
+  double ttm;
+  double riskfreeinterest;
   double volatility;
+}
+
+double CallBlackScholesFormula(const struct * OC) {
+  double S = OC->spotprice;
+  double K = OC ->strikeprice;
+  double T = OC->ttm;
+  double r = OC->riskfreeinterest;
+  double V = OC->volatility;
+  d1 = (std::log(S/K) + (r+0.5*V*V) * T) / (V*std::sqrt(T));
+  d2 = d1 - V * std::sqrt(T);
+  return S * (0.5*std::erfc(-d1/std::sqrt(2.0))) - K * std::exp(-r*T) * (0.5*std::erfc(-d2/std::sqrt(2.0)));
+}
+
+double PutBlackScholesFormula(const struct * OC) {
+  double S = OC->spotprice;
+  double K = OC ->strikeprice;
+  double T = OC->ttm;
+  double r = OC->riskfreeinterest;
+  double V = OC->volatility;
+  d1 = (std::log(S/K) + (r+0.5*V*V) * T) / (V*std::sqrt(T));
+  d2 = d1 - V * std::sqrt(T);
+  return K * (0.5*std::erfc(-d1/std::sqrt(2.0))) - S * std::exp(-r*T) * (0.5*std::erfc(-d2/std::sqrt(2.0)));
 }
