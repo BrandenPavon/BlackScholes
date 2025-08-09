9f0e295b89db7adfaa64c2e910108e81bc213a1d
bp <bp@bpsite.xyz>
2025-08-09 12:57:12 -0700

    fix: input specification

--- main.cpp
+++ main.cpp
@@ -8,11 +8,11 @@ int main(void) {
     std::cin >> o.spotprice;
     std::cout << "Strike Price: $";
     std::cin >> o.strikeprice;
-    std::cout << "Time to Maturity: ";
+    std::cout << "Time to Maturity in years(e.g 0.5 is 6 months): ";
     std::cin >> o.ttm;
-    std::cout << "Risk Free Interest: ";
+    std::cout << "Risk Free Interest as decimal (e.g 0.05 is 5%): ";
     std::cin >> o.riskfreeinterest;
-    std::cout << "Volatility: ";
+    std::cout << "Volatility as decimal (e.g 0.05 is 5%) ";
     std::cin >> o.volatility;
 
     std::cout << CallBlackScholesFormula(o) << std::endl;
